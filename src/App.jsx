c175c2f3a9991c6bddbac96814505b19d8217c0a
ImK4Lok <samkalok107@gmail.com>
2022-11-21 16:26:32 +0800

    update style syntax and fixed bottom cart section

--- src/App.jsx
+++ src/App.jsx
@@ -137,7 +137,7 @@ function App() {
         <section id="main">
           <div className="relative h-[500px] grid grid-cols-7 pb-48 mt-7">
             {/* Left Scroll Section */}
-            <div id="left-scroll-parent" className="h-[100%] col-span-2 flex flex-col bg-zinc-50 overflow-y-scroll overflow-x-hidden scrollbar-hide">
+            <div id="left-scroll-parent" className="h-full col-span-2 flex flex-col bg-zinc-50 overflow-y-scroll overflow-x-hidden scrollbar-hide">
               <ul>
                 {data.map((item, index) => {
                   return (
@@ -205,13 +205,13 @@ function App() {
             </div>
 
             {/* Cart Section */}
-            <div className="absolute bottom-6 w-full px-4 z-20">
+            <div className="fixed bottom-6 w-full px-11 z-20 left-0 mx-auto">
               <div onClick={() => setShowCartModal(true)} className=" relative flex justify-between items-center p-1 inset-0 h-full w-full bg-[#3F3E5D] rounded-lg cursor-pointer">
                 <div className="flex">
                   {/* <svg className="w-12 h-12 text-[#FA6B16] -rotate-[14deg]" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
                   </svg> */}
-                  <svg className="w-10 h-10 text-[#FA6B16] -rotate-[8deg] translate-x-1 select-none cursor-default" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24" strokeWidth={1.5} stroke="white">
+                  <svg className="w-10 h-10 text-[#FA6B16] -rotate-[8deg] translate-x-1 -translate-y-1 select-none cursor-default" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24" strokeWidth={1.5} stroke="white">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 10.5V6a3.75 3.75 0 10-7.5 0v4.5m11.356-1.993l1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 01-1.12-1.243l1.264-12A1.125 1.125 0 015.513 7.5h12.974c.576 0 1.059.435 1.119 1.007zM8.625 10.5a.375.375 0 11-.75 0 .375.375 0 01.75 0zm7.5 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
                   </svg>
 
